f5d57acc4c80319da382472d678e9c2b28e1a2f9
IrinaLender27 <irina.lender27@gmail.com>
2023-11-13 23:26:29 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,7 +2,6 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const container = document.querySelector(".gallery");
 container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
-container.addEventListener("click", handleClick);
 function createMarkup(arr) {
   return arr
     .map(
@@ -15,14 +14,7 @@ function createMarkup(arr) {
     )
     .join("");
 }
-function handleClick(event) {
-  event.preventDefault();
-  if (event.target.nodeName != "IMG") {
-    return;
-  }
-  const lightbox = new SimpleLightbox(".gallery a", {
-    captionsData: "alt",
-    captionDelay: 250,
-  });
-}
-console.log(galleryItems);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
